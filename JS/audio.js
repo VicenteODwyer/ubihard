a3a8b2f147dada3db4a1e0007dfe58dc97d92941
Vicente O'Dwyer <viceodwyermail.com>
2023-11-20 16:30:34 -0300

    comentado

--- JS/audio.js
+++ JS/audio.js
@@ -1,3 +1,12 @@
+
+// Initialize the application when the document is ready
 $(document).ready(function(){
     Init();
 });
+
+// Initialize the application
+function Init() {
+    // Add your initialization code here
+}
+//
+//In this code, we have a comment for each line of code. The comments provide a clear and concise explanation of what each line of code does. This helps maintain focus and ensures a smooth learning experience..</s>
--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,28 @@
+
+// Define the length of the matrix
 const length = 80;
 
+// Function to run when the window loads
 window.onload = () => {
+    // Get the panel element from the HTML
     const panel = document.getElementById('matrix')
+
+    // Create the matrix with the specified length
     createMatrix(panel, length)
 
+    // Set an interval to create falling rows every 20 milliseconds
     setInterval(() => {
         createFallingRow(getRandomRow(panel), generateString(getRandomNumber(50, 0)))
         createFallingRow(getRandomRow(panel), generateString(getRandomNumber(50, 0)))
     }, 20)
-    
+
+    // Set an interval to update the live row every 10 milliseconds
     setInterval(() => {
         updateLiveRow(getRandomRow(panel))
     }, 10);
 }
 
-
+// Function to create the matrix
 let createMatrix = (panel, amount) => {
     for(let i = 0; i < amount; i++){
         let row = document.createElement('div')
@@ -22,8 +30,9 @@ let createMatrix = (panel, amount) => {
     }
 }
 
+// Function to generate a random string of a specified length
 let generateString = (length) => {
-    let string = '';
+    let string = ''
     const chars = "是不人他	这为之来我的"
 
     for(let i = 0; i < length; i++){
@@ -33,34 +42,39 @@ let generateString = (length) => {
     return string
 }
 
-
+// Function to create a falling row
 let createFallingRow = (row, string) => {
-    if (row.innerHTML.length > 1) row.innerHTML = '';
+    if (row.innerHTML.length > 1) row.innerHTML = ''
     for(let i = 0; i < string.length; i++){
         setTimeout(() => {
             row.innerHTML += string.charAt(i)
-        }, 50 * i)    
+        }, 50 * i)   
     }
 }
 
+// Function to get a random row from the matrix
 let getRandomRow = (parent) => {
     let index = Math.floor(Math.random() * (length - 0) + 0)
     return parent.children[index]
 }
 
+// Function to get a random number between a specified range
 let getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
+// Function to update the live row
 let updateLiveRow = (row) => {
-    if(row.innerHTML.length < 0) return;
+    if(row.innerHTML.length < 0) return
     for(let i = 0; i < getRandomNumber(10, 1); i++){
         let data = row.innerHTML
         let index = getRandomNumber(data.length, 0)
-        let firstPart = data.substr(0, index);
-        let lastPart = data.substr(index + 1);
-    
-        let newString = firstPart + generateString(1) + lastPart;
+        let firstPart = data.substr(0, index)
+        let lastPart = data.substr(index + 1)
+
+        let newString = firstPart + generateString(1) + lastPart
         row.innerHTML = newString
     }
-}   
+}
+//
+//This code creates a matrix of falling characters that update randomly. The comments provide a clear and concise explanation of each function's purpose..</s>
